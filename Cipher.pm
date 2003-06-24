77bd070aae9b9d7634776d6c706ac5409201f876
Robert Fischer <chia@cpan.org>
2003-06-24 04:50:31 +0000

    Import of CHIA/Crypt-Cipher-0.03 from CPAN.
    gitpan-cpan-distribution: Crypt-Cipher
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         CHIA/Crypt-Cipher-0.03.tar.gz
    gitpan-cpan-author:       CHIA
    gitpan-cpan-maturity:     released

--- Cipher.pm
+++ Cipher.pm
@@ -14,39 +14,14 @@ use strict;
 use warnings;
 use Carp;
 
-# Implementation-specific imports
-use Regexp::Tr v0.05;
+BEGIN {
+    eval { require Text::Cipher; };
+    carp "Please upgrade Crypt::Cipher to Text::Cipher.\n" if($@);
+}
 
 # UNIVERSAL class variables
-our @ISA = qw(Regexp::Tr);
-our $VERSION = '0.02';
-
-BEGIN {
-    *new             = \&Regexp::Tr::new;
-    *clean           = \&Regexp::Tr::flush;
-    *encipher_string = \&encipher;
-    *encipher_scalar = \&Regexp::Tr::bind;
-}
-
-sub encipher {
-    my($self,$val) = (shift,shift);
-    Regexp::Tr::bind($self, \$val);
-    return $val;
-}
-
-sub encipher_list {
-    my $self = shift;
-    return map { scalar($self->encipher($_)) } @_;
-}
-
-sub encipher_array {
-    my($self,$ref) = @_;
-    carp "Parameter passed is not an array ref" unless(ref($ref) eq "ARRAY");
-    my @refarray = \(@{$ref});
-    eval { map { scalar($self->encipher_scalar($_)) } @refarray };
-    die "Error in encipher_array: $@" if($@);
-}
-
+our @ISA = qw(Text::Cipher);
+our $VERSION = '0.03';
 
 return 1;
 __END__
@@ -54,179 +29,22 @@ __END__
 
 =head1 NAME
 
-Crypt::Cipher - Very flexible base class for text ciphers. 
-
-=head1 SYNOPSIS
-
-  # WITHIN MAIN
-  use Crypt::Cipher;
-  my $obj = Crypt::Cipher->new($domain,$mapping);      #Constructor
-  my $storage = $obj->encipher("some string");         #String op
-  $obj->encipher_scalar(\$some_scalar);                #Scalar ref op
-  my @big_storage = $obj->encipher_list("2nd string",  #List operator
-                                        "and another",
-                                        "and more",   
-                                        "yet more");
-  $obj->encipher_array(\@some_array);                  #Array operator
-  Crypt::Cipher->clean();                              #Memory cleanup
-  
-  # AS INHERITED BASE CLASS
-  package Crypt::Cipher::NewCipher;
-  @ISA = qw(Crypt::Cipher);
-   
-  sub new {
-      my $class = shift;
-      ...
-      ...
-      return $class->SUPER::new($from,$to);
-  }
-
-  # Crypt::Cipher::NewCipher is automatically supplied the cipher
-  # methods.
-
-  # Aliasing example: make Crypt::Cipher::NewCipher::flush operate 
-  # just like Crypt::Cipher::clean.
-  BEGIN { *flush = \&Crypt::Cipher::clean };  
-
+Crypt::Cipher - Depreciated -- see Text::Cipher
 
 =head1 ABSTRACT
 
 Provides a standard interface and simple methods for ciphers of 
 various kinds, saving on development time and redundant code.
 
-=head1 DESCRIPTION
-
-=head2 Use as an Independent Class
-
-=over 4
-
-=head3 Crypt::Cipher->new(PARAMLIST)
-
-=over 4
-
-This method is the constructor for the Crypt::Cipher class.  When
-called as Crypt::Cipher->new(DOMAIN, MAPPING, MODS), it creates an
-object mapping each letter in DOMAIN to its respective letter in 
-MAPPING.  MODS are modifieres to the cipher as per the tr/// operator.
-
-=back
-
-=head3 $obj->encipher(STRING)
-
-=head3 $obj->encipher_string(STRING)
-
-=over 4
-
-Performs the cipher on the string and returns the enciphered value.
-
-=back
-
-=head3 $obj->encipher_scalar(SCALARREF)
-
-=over 4
-
-This method takes a reference to a scalar (note that it does not 
-B<create> the reference to the scalar) and performs the cipher upon 
-the scalar it refers to.  It returns true if anything in the scalar 
-was changed through the application of the cipher.
-
-=back
-
-=head3 $obj->encipher_list(LIST)
-
-=over 4
-
-Returns a list of every element of LIST enciphered.
-
-=back
-
-=head3 $obj->encipher_array(ARRAYREF)
-
-=over 4
-
-Enciphers every element in the array pointed to by ARRAYREF.
-
-=back
-
-=head3 Crypt::Cipher->clean()
-
-=over 4
-
-Performs operations to recover memory, which may or may not make a
-substantial change.
-
-=back
-
-=back
-
-=head2 Use as a Base Class
-
-=over 4
-
-=head3 Default Use
-
-=over 4
-
-If you just want to use the methods provided to you by the class,
-all you have to do is end your constructor with the following code
-snippet:
-
-    return $class->SUPER::new($from,$to);
-
-The $class variable should be the name of your class, and $from should
-contain the letters your cipher will change (aka: SEARCHLIST) while 
-$to should contain the letters your cipher will move things over to
-(aka: REPLACEMENTLIST).
-
-=back
-
-=head3 Overloading
-
-=over 4
-
-If you want to overload a method (the "new" method is popular to
-overload, as is the "clean" method), then just be sure to end your new
-method with a call to this class's method.
-
-    # Example
-    sub encipher {
-        my $obj = shift;
-        ...
-        return $obj->SUPER::encipher(@params);
-    }
-
-If you are trying to do something even fancier, please ensure that any
-other impelementation of Crypt::Cipher or any other cipher built on 
-Crypt::Cipher and your code would still function.
-
-=back
-
-=back
-
-=head1 HISTORY
-
-=over 8
-
-=item 0.02
-
-Fixed minor inheritance bugs and documentation.  Also fixed a major
-bug occuring when used in list context.
-
-=item 0.01
-
-Original version; created by h2xs 1.22 with options
-
-  -ABCX
-	-n
-	Crypt::Cipher
-
-=back
+This class has now been replaced with L<Text::Cipher>.  For 
+compatability purposes, this class is now an infintesimally thin 
+wrapper around that class.
 
 =head1 SEE ALSO
 
 =over 4
 
-=item * B<L<Text::Shift>Z<>>: One cipher built on this system
+=item * B<L<Text::Cipher>Z<>>: The newest edition.
 
 =back 4
 
